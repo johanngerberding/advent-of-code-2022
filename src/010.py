7f37d2a2a4003502bc42e1d661d7a693aa8a7b00
johanngerberding <johann.gerberding@gmail.com>
2022-12-11 20:25:58 +0100

    started working on day 10

--- src/010.py
+++ src/010.py
@@ -1,7 +1,42 @@
+class CPU: 
+    def __init__(self, register: int): 
+        self.register = register
+        self.cycle = 0 
+    
+    def execute(self, inst): 
+        if inst == "noop": 
+            self.cycle += 1 
+        else: 
+            self.cycle += 1 
+            self.addx(inst)  
+
+    def addx(self, inst): 
+        self.cycle += 1 
+        self.register += int(inst.split(" ")[1])
+
+def main():
+    inp = "../inputs/010.txt" 
+    with open(inp, 'r') as fp: 
+        data = [el.strip() for el in fp.readlines()]
+
+    register = 1 
+    cycle = 0 
+
+    
+    strength = [] 
+    
+    for inst in data: 
+        if inst == "noop": 
+            cycle += 1  
+        else:
+            ins = inst.split(" ")[0]
+            val = int(inst.split(" ")[1])
+            cycle += 1
+                
+            cycle += 1 
+            register += val  
 
 
-def main(): 
-    pass 
 
 
 if __name__ == "__main__":
