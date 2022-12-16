469cfc536b1830660f9028c7a63357d2acb112da
johanngerberding <johann.gerberding@gmail.com>
2022-12-16 17:41:16 +0100

    day 10 part 1

--- src/010.py
+++ src/010.py
@@ -1,41 +1,36 @@
 class CPU: 
     def __init__(self, register: int): 
         self.register = register
-        self.cycle = 0 
+        self.cycle = 0
+        self.strengths = [] 
     
     def execute(self, inst): 
         if inst == "noop": 
-            self.cycle += 1 
+            self.increment_cycle()
         else: 
-            self.cycle += 1 
+            self.increment_cycle() 
             self.addx(inst)  
 
     def addx(self, inst): 
-        self.cycle += 1 
+        self.increment_cycle() 
         self.register += int(inst.split(" ")[1])
+    
+    def increment_cycle(self): 
+        self.cycle += 1
+        if self.cycle in [20, 60, 100, 140, 180, 220]: 
+            self.strengths.append(self.register * self.cycle) 
 
 def main():
     inp = "../inputs/010.txt" 
     with open(inp, 'r') as fp: 
         data = [el.strip() for el in fp.readlines()]
 
-    register = 1 
-    cycle = 0 
-
-    
-    strength = [] 
+    cpu = CPU(1) 
     
     for inst in data: 
-        if inst == "noop": 
-            cycle += 1  
-        else:
-            ins = inst.split(" ")[0]
-            val = int(inst.split(" ")[1])
-            cycle += 1
-                
-            cycle += 1 
-            register += val  
+        cpu.execute(inst)  
 
+    print(f"Solution Part 1: {sum(cpu.strengths)}")
 
 
 
