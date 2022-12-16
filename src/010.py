8d579522b4439cd43df42f292f8b3464764eccba
johanngerberding <johann.gerberding@gmail.com>
2022-12-16 19:35:54 +0100

    day 10 part 2

--- src/010.py
+++ src/010.py
@@ -20,6 +20,48 @@ class CPU:
         if self.cycle in [20, 60, 100, 140, 180, 220]: 
             self.strengths.append(self.register * self.cycle) 
 
+
+class CPU2: 
+    def __init__(self):
+        self.register = 1 
+        self.cycle = 0 
+        self.pixels = []
+        self.row = [] 
+    
+    def execute(self, inst): 
+        if inst == "noop": 
+            self.increment_cycle()
+        else: 
+            self.increment_cycle() 
+            self.addx(inst)  
+
+    def addx(self, inst): 
+        self.increment_cycle() 
+        self.register += int(inst.split(" ")[1])
+    
+    def increment_cycle(self): 
+        if self.in_range(): 
+            self.row.append('#')
+        else: 
+            self.row.append('.') 
+        self.cycle += 1
+        if self.cycle == 40: 
+            self.pixels.append(self.row)
+            self.row = []
+            self.cycle = 0 
+         
+    def in_range(self): 
+        if self.cycle >= self.register - 1 and self.cycle <= self.register + 1: 
+            return True 
+        return False 
+
+    def render(self): 
+        for row in self.pixels: 
+            line = ""
+            for p in row: 
+                line += p 
+            print(line)
+
 def main():
     inp = "../inputs/010.txt" 
     with open(inp, 'r') as fp: 
@@ -31,6 +73,13 @@ def main():
         cpu.execute(inst)  
 
     print(f"Solution Part 1: {sum(cpu.strengths)}")
+
+    cpu2 = CPU2()
+
+    for inst in data: 
+        cpu2.execute(inst)
+    
+    cpu2.render()
 
 
 
