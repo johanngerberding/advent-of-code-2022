75fd2f2ec5d7c8c8e8d6731aa1cc31838ce0bfef
johanngerberding <johann.gerberding@gmail.com>
2022-12-31 17:32:30 +0100

    struggling with day 16

--- src/016.py
+++ src/016.py
@@ -1,9 +1,14 @@
+from collections import deque
 
 
 class PressureMonitor: 
     def __init__(self):
         self.pressure = 0 
-        self.time = 0 
+        self.time = 0
+        self.history = [0] 
+    
+    def update(self): 
+        self.history.append(self.pressure)
 
 
 class Valve:
@@ -12,9 +17,52 @@ class Valve:
         self.flow_rate = flow_rate
         self.adjs = connections
         self.pm = pm
+        self.open = False
+        self.travel_count = 0  
 
     def __str__(self): 
         return f"Valve {self.name} | Flow rate: {self.flow_rate} | Connections: {self.adjs}"
+    
+    def opening(self):
+        self.travel_count += 1 
+        if not self.open: 
+            self.pm.time += 1 
+            self.open = True 
+            print(f"Mins: {self.pm.time} | Opening valve {self.name} | pressure: {self.pm.pressure}") 
+            self.pm.pressure += self.flow_rate 
+            self.pm.update()
+
+
+def get_next(valves: dict, valve: str) -> str: 
+    """Return next non opened adjacent valve. 
+    If all are open, return the one with the minimum visits""" 
+    adjs = valves[valve].adjs   
+    for adj in adjs: 
+        if not valves[adj].open:
+            return adj
+
+    tcounts = [adj.travel_count for adj in adjs]
+    min_count = min(tcounts)
+
+    return adjs[tcounts.index(min_count)]
+
+
+
+def dfs(valves: dict, root: str, visited: set, mins: int): 
+    valve = valves[root] 
+    visited.add(valve.name)
+    valve.pm.time += 1
+    valve.pm.update()
+    print(f"Mins: {valve.pm.time} | Moving to valve {valve.name} | pressure: {valve.pm.pressure}") 
+    if valve.pm.time >= mins: 
+        return  
+    valve.opening()
+    if valve.pm.time >= mins: 
+        return
+    for adj in valve.adjs: 
+        if adj not in visited: 
+            dfs(valves, adj, visited, mins) 
+
 
 def parse(line: str): 
     line = line.split(";") 
@@ -24,18 +72,23 @@ def parse(line: str):
         adjs = [el.strip() for el in line[1].split("valves")[1].split(",")]
     elif "valve" in line[1]: 
         adjs = [line[1].split("valve")[1].strip()]
-    return Valve(name, rate, adjs, pm)
+    return name, Valve(name, rate, adjs, pm)
 
-with open("../inputs/016.txt", 'r') as fp: 
+with open("../inputs/016_test.txt", 'r') as fp: 
     data = [el.strip() for el in fp.readlines()]
 
 pm = PressureMonitor()
 
-valves = []
+valves = {} 
 for line in data: 
-    valves.append(parse(line))
+    name, valve = parse(line)
+    valves[name] = valve 
 
-for valve in valves: 
+for valve in valves.values(): 
     print(valve)
 
-# exhaustive search, check all possible ways for 30 mins 
+mins = 30 
+root = "AA"
+visited = set()
+
+dfs(valves, root, visited, mins) 
